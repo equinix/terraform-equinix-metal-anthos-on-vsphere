b542a0cd3a0bf69cac4dd9d592b09b1aa8e45b96
Paul Mason <paulrmason@paulrmason-macbookpro.roam.corp.google.com>
2020-02-24 14:50:04 -0800

    added new information to readme and corrected the boolean option

--- 31-anthos-gen-ssh-keys.tf
+++ 31-anthos-gen-ssh-keys.tf
@@ -1,11 +1,9 @@
 resource "tls_private_key" "anthos_ssh_key" {
-  count = var.anthos_deploy_workstation_prereqs ? 1 : 0
   algorithm = "RSA"
   rsa_bits = 4096
 }
 
 resource "local_file" "anthos_priv_key" {
-    count = var.anthos_deploy_worksation_prereqs ? 1 : 0
     content = chomp(tls_private_key.anthos_ssh_key.private_key_pem)
     filename = "anthos_ssh_priv_key"
     file_permission = "0600"
