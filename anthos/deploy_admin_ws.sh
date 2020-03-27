a3c7dfcb456163d89ac747b8802dfa4fb2262286
Paul Mason <44103209+paemason@users.noreply.github.com>
2020-03-27 09:27:30 -0700

    change deploy_admin_ws.sh
    use the --ssh-key-path flag with the gkeadm create admin-workstation command instead of the convoluted copy key process.

--- anthos/deploy_admin_ws.sh
+++ anthos/deploy_admin_ws.sh
@@ -5,9 +5,7 @@ export WORKSTATIONIP=__IP_ADDRESS__
 if [ -f "/root/anthos/gkeadm" ] ; then
   echo "Deploying Admin Workstation with gkeadm"
   openssl s_client -showcerts -verify 5 -connect ${vcenter_fqdn}:443 < /dev/null | awk '/BEGIN/,/END/{ if(/BEGIN/){a++}; out="/root/anthos/vspherecert.pem"; print >out}'
-  /root/anthos/gkeadm create admin-workstation --config /root/anthos/admin-ws-config.yaml
-  scp -i /root/.ssh/gke-admin-workstation /root/anthos/ssh_key.pub ubuntu@$WORKSTATIONIP:/home/ubuntu/.ssh/ssh_key.pub
-  ssh -i /root/.ssh/gke-admin-workstation ubuntu@$WORKSTATIONIP 'echo "$(cat /home/ubuntu/.ssh/ssh_key.pub)" >> /home/ubuntu/.ssh/authorized_keys'
+  /root/anthos/gkeadm create admin-workstation --config /root/anthos/admin-ws-config.yaml --ssh-key-path /root/anthos/ssh_key
 else
   echo "Deploying Admin Workstation with terraform"
   terraform init
