d7009667ee91c32b1f9ecf3d111b97393cd7bf86
Paul Mason <44103209+paemason@users.noreply.github.com>
2020-04-09 13:25:30 -0700

    skip gkeadm validation
    resolves #54

--- anthos/deploy_admin_ws.sh
+++ anthos/deploy_admin_ws.sh
@@ -5,7 +5,7 @@ export WORKSTATIONIP=__IP_ADDRESS__
 if [ -f "/root/anthos/gkeadm" ] ; then
   echo "Deploying Admin Workstation with gkeadm"
   openssl s_client -showcerts -verify 5 -connect ${vcenter_fqdn}:443 < /dev/null | awk '/BEGIN/,/END/{ if(/BEGIN/){a++}; out="/root/anthos/vspherecert.pem"; print >out}'
-  /root/anthos/gkeadm create admin-workstation --config /root/anthos/admin-ws-config.yaml --ssh-key-path /root/anthos/ssh_key
+  /root/anthos/gkeadm create admin-workstation --config /root/anthos/admin-ws-config.yaml --ssh-key-path /root/anthos/ssh_key --skip-validation
 else
   echo "Deploying Admin Workstation with terraform"
   terraform init
