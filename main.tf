23c4403731fd05f44e04aa5073aa2bad65a1c1db
Marques Johansson <mjohansson@equinix.com>
2021-05-18 11:54:24 -0400

    enable mmodule_variable_optional_attrs
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- main.tf
+++ main.tf
@@ -1,27 +1,3 @@
-terraform {
-  required_providers {
-    metal = {
-      source  = "equinix/metal"
-      version = "~> 2.0"
-    }
-    local = {
-      source = "hashicorp/local"
-    }
-    null = {
-      source = "hashicorp/null"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-    template = {
-      source = "hashicorp/template"
-    }
-    tls = {
-      source = "hashicorp/tls"
-    }
-  }
-}
-
 provider "metal" {
   auth_token = var.auth_token
 }
--- versions.tf
+++ versions.tf
@@ -1,3 +1,25 @@
 terraform {
+  experiments = [module_variable_optional_attrs]
   required_version = ">= 0.14"
+  required_providers {
+    metal = {
+      source  = "equinix/metal"
+      version = "~> 2.0"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+    tls = {
+      source = "hashicorp/tls"
+    }
+  }
 }
